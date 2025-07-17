4ea520b772a5f21ee265fb38b0944ba55bc472f1
LBJhui <690306059@qq.com>
2025-07-17 16:41:28 +0800

    2025-07-17

---
+++ source-code/webpack5/src/js/sum.js
@@ -0,0 +1,5 @@
+function sum(...args) {
+  return args.reduce((a, b) => a + b, 0)
+}
+
+export default sum
---
+++ source-code/webpack5/src/main.js
@@ -0,0 +1,13 @@
+// 入口文件
+// js 文件后缀名可以不写
+import count from './js/count'
+import sum from './js/sum'
+
+// 想要 webpack 打包资源，必须引入该资源
+
+import './css/index.css'
+import './less/index.less'
+import './sass/index.scss'
+
+console.log(count(2, 1))
+console.log(sum(1, 2, 3, 4))
