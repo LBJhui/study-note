54c395fb260f39af6f7512f07bd789fcf8d1de8a
LBJhui <690306059@qq.com>
2025-07-24 16:38:45 +0800

    2025-07-24

--- code/vue-vite-pnpm/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <RouterView></RouterView>
-</template>
-
-<script setup lang="ts">
-document.title = 'LBJ辉'
-</script>
-
-<style scoped></style>
--- source-code/vue3_admin/scritps/preinstall.js
+++ source-code/vue3_admin/scritps/preinstall.js
@@ -1,5 +1,9 @@
+// 如果安装依赖使用的包管理工具不是 pnpm，则在终端控制台中打印警告，然后退出本次执行的命令
 if (!/pnpm/.test(process.env.npm_execpath || '')) {
   console.warn(`\u001b[33mThis repository must using pnpm as the package manager ` + ` for scripts to work properly.\u001b[39m\n`)
   process.exit(1)
 }
-// "preinstall": "node ./scripts/preinstall.js"
+
+/**
+ * 在 package.json 文件 scripts 中添加以下脚本 "preinstall": "node ./scripts/preinstall.js"
+ */
