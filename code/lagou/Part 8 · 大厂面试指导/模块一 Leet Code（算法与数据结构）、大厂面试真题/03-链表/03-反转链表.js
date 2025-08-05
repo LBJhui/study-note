13030a9e2fa48cbdf30336cbd3c8713bd64fd0e8
LBJhui <690306059@qq.com>
2025-08-05 16:27:09 +0800

    2025-08-05

---
+++ code/lagou/Part 8 · 大厂面试指导/模块一 Leet Code（算法与数据结构）、大厂面试真题/03-链表/03-反转链表.js
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.cn/problems/reverse-linked-list/description/
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  // 声明变量记录 prev、cur
+  let prev = null
+  let cur = head
+  // 当 cur 是节点时，进行迭代
+  while (cur) {
+    // 先保存当前节点的下一个节点
+    const next = cur.next
+    cur.next = prev
+    prev = cur
+    cur = next
+  }
+  return prev
+}
