b5339ae61913363ddf564fa7083a44af935d3b50
LBJhui <690306059@qq.com>
2025-08-08 16:35:19 +0800

    2025-08-08

--- source-code/vue3_admin/src/views/dashboard/index.vue
+++ source-code/vue3_admin/src/views/dashboard/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>dashboard</div>
+  <div
+    >dashboard
+    <l-link></l-link>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LLink from '@/L-UI/L-Link/index.vue'
+</script>
 
 <style scoped lang=""></style>
